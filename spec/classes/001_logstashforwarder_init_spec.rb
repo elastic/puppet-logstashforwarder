d561abbbfa8198ff6ad5de8da6b0cce7d8239cd3
Richard Pijnenburg <richard@ispavailability.com>
2015-03-30 11:32:20 +0000

    Improve basic testing

--- spec/classes/001_logstashforwarder_init_spec.rb
+++ spec/classes/001_logstashforwarder_init_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe 'logstashforwarder', :type => 'class' do
 
-  [ 'RedHat', 'CentOS', 'Fedora', 'Scientific', 'Amazon', 'OracleLinux' ].each do |distro|
-
     default_params = {
       :servers  => [ '192.168.0.1' ],
       :ssl_ca   => '/path/to/ssl.ca',
@@ -11,12 +9,28 @@ describe 'logstashforwarder', :type => 'class' do
       :ssl_cert => '/path/to/ssl.cert'
     }
 
-    context "on #{distro} OS" do
+  on_supported_os.each do |os, facts|
 
-      let :facts do {
-        :operatingsystem => distro,
-        :kernel => 'Linux'
-      } end
+    context "on #{os} OS" do
+
+      case facts[:osfamily]
+      when 'Debian'
+        let(:defaults_path) { '/etc/default' }
+        let(:pkg_ext) { 'deb' }
+        let(:pkg_prov) { 'dpkg' }
+      when 'RedHat'
+        let(:defaults_path) { '/etc/sysconfig' }
+        let(:pkg_ext) { 'rpm' }
+        let(:pkg_prov) { 'rpm' }
+      when 'Suse'
+        let(:defaults_path) { '/etc/sysconfig' }
+        let(:pkg_ext) { 'rpm' }
+        let(:pkg_prov) { 'rpm' }
+      end
+
+      let (:facts) {
+        facts
+      }
 
       let (:params) {
         default_params   
@@ -52,7 +66,7 @@ describe 'logstashforwarder', :type => 'class' do
 
             let (:params) {
               default_params.merge({
-                :version => '1.0'
+              :version => '1.0'
               })
             }
 
@@ -63,7 +77,7 @@ describe 'logstashforwarder', :type => 'class' do
 
             let (:params) {
               default_params.merge({
-                :autoupgrade => true
+              :autoupgrade => true
               })
             }
 
@@ -75,7 +89,7 @@ describe 'logstashforwarder', :type => 'class' do
             let (:params) {
               default_params.merge({
                 :version     => '0.90.10',
-                :package_url => 'puppet:///path/to/some/logstashforwarder-0.90.10.rpm'
+                :package_url => 'puppet:///path/to/some/logstash-forwarder-0.90.10.#{pkg_ext}'
               })
             }
 
@@ -90,70 +104,70 @@ describe 'logstashforwarder', :type => 'class' do
 
             let (:params) {
               default_params.merge({
-                :package_url => 'puppet:///path/to/package.rpm'
+                :package_url => "puppet:///path/to/package.#{pkg_ext}"
               })
             }
 
             it { should contain_exec('create_package_dir_logstashforwarder').with(:command => 'mkdir -p /opt/logstashforwarder/swdl') }
-            it { should contain_file('/opt/logstashforwarder/swdl').with(:purge => false, :force => false, :require => "Exec[create_package_dir_logstashforwarder]") }
-            it { should contain_file('/opt/logstashforwarder/swdl/package.rpm').with(:source => 'puppet:///path/to/package.rpm', :backup => false) }
-            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => '/opt/logstashforwarder/swdl/package.rpm', :provider => 'rpm') }
+            it { should contain_file('/opt/logstashforwarder/swdl/').with(:purge => false, :force => false, :require => "Exec[create_package_dir_logstashforwarder]") }
+            it { should contain_file("/opt/logstashforwarder/swdl/package.#{pkg_ext}").with(:source => "puppet:///path/to/package.#{pkg_ext}", :backup => false) }
+            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => "/opt/logstashforwarder/swdl/package.#{pkg_ext}", :provider => pkg_prov) }
           end
 
           context 'using http:// schema' do
 
-            let (:params) { 
+            let (:params) {
               default_params.merge({
-              :package_url => 'http://www.domain.com/path/to/package.rpm'
+                :package_url => "http://www.domain.com/path/to/package.#{pkg_ext}"
               })
-            }
+            } 
 
             it { should contain_exec('create_package_dir_logstashforwarder').with(:command => 'mkdir -p /opt/logstashforwarder/swdl') }
-            it { should contain_file('/opt/logstashforwarder/swdl').with(:purge => false, :force => false, :require => "Exec[create_package_dir_logstashforwarder]") }
-            it { should contain_exec('download_package_logstashforwarder').with(:command => 'wget -O /opt/logstashforwarder/swdl/package.rpm http://www.domain.com/path/to/package.rpm 2> /dev/null', :require => 'File[/opt/logstashforwarder/swdl]') }
-            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => '/opt/logstashforwarder/swdl/package.rpm', :provider => 'rpm') }
+            it { should contain_file('/opt/logstashforwarder/swdl/').with(:purge => false, :force => false, :require => "Exec[create_package_dir_logstashforwarder]") }
+            it { should contain_exec('download_package_logstashforwarder').with(:command => "wget -O /opt/logstashforwarder/swdl/package.#{pkg_ext} http://www.domain.com/path/to/package.#{pkg_ext} 2> /dev/null", :require => 'File[/opt/logstashforwarder/swdl]') }
+            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => "/opt/logstashforwarder/swdl/package.#{pkg_ext}", :provider => pkg_prov) }
           end
 
           context 'using https:// schema' do
 
-            let (:params) { 
+            let (:params) {
               default_params.merge({
-              :package_url => 'https://www.domain.com/path/to/package.rpm'
+                :package_url => "https://www.domain.com/path/to/package.#{pkg_ext}"
               })
             }
 
             it { should contain_exec('create_package_dir_logstashforwarder').with(:command => 'mkdir -p /opt/logstashforwarder/swdl') }
             it { should contain_file('/opt/logstashforwarder/swdl').with(:purge => false, :force => false, :require => 'Exec[create_package_dir_logstashforwarder]') }
-            it { should contain_exec('download_package_logstashforwarder').with(:command => 'wget -O /opt/logstashforwarder/swdl/package.rpm https://www.domain.com/path/to/package.rpm 2> /dev/null', :require => 'File[/opt/logstashforwarder/swdl]') }
-            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => '/opt/logstashforwarder/swdl/package.rpm', :provider => 'rpm') }
+            it { should contain_exec('download_package_logstashforwarder').with(:command => "wget -O /opt/logstashforwarder/swdl/package.#{pkg_ext} https://www.domain.com/path/to/package.#{pkg_ext} 2> /dev/null", :require => 'File[/opt/logstashforwarder/swdl]') }
+            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => "/opt/logstashforwarder/swdl/package.#{pkg_ext}", :provider => pkg_prov) }
           end
 
           context 'using ftp:// schema' do
 
-            let (:params) { 
+            let (:params) {
               default_params.merge({
-              :package_url => 'ftp://www.domain.com/path/to/package.rpm'
+                :package_url => "ftp://www.domain.com/path/to/package.#{pkg_ext}"
               })
             }
 
             it { should contain_exec('create_package_dir_logstashforwarder').with(:command => 'mkdir -p /opt/logstashforwarder/swdl') }
             it { should contain_file('/opt/logstashforwarder/swdl').with(:purge => false, :force => false, :require => 'Exec[create_package_dir_logstashforwarder]') }
-            it { should contain_exec('download_package_logstashforwarder').with(:command => 'wget -O /opt/logstashforwarder/swdl/package.rpm ftp://www.domain.com/path/to/package.rpm 2> /dev/null', :require => 'File[/opt/logstashforwarder/swdl]') }
-            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => '/opt/logstashforwarder/swdl/package.rpm', :provider => 'rpm') }
+            it { should contain_exec('download_package_logstashforwarder').with(:command => "wget -O /opt/logstashforwarder/swdl/package.#{pkg_ext} ftp://www.domain.com/path/to/package.#{pkg_ext} 2> /dev/null", :require => 'File[/opt/logstashforwarder/swdl]') }
+            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => "/opt/logstashforwarder/swdl/package.#{pkg_ext}", :provider => pkg_prov) }
           end
 
           context 'using file:// schema' do
 
-            let (:params) { 
+            let (:params) {
               default_params.merge({
-              :package_url => 'file:/path/to/package.rpm'
+                :package_url => "file:/path/to/package.#{pkg_ext}"
               })
             }
 
             it { should contain_exec('create_package_dir_logstashforwarder').with(:command => 'mkdir -p /opt/logstashforwarder/swdl') }
             it { should contain_file('/opt/logstashforwarder/swdl').with(:purge => false, :force => false, :require => 'Exec[create_package_dir_logstashforwarder]') }
-            it { should contain_file('/opt/logstashforwarder/swdl/package.rpm').with(:source => '/path/to/package.rpm', :backup => false) }
-            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => '/opt/logstashforwarder/swdl/package.rpm', :provider => 'rpm') }
+            it { should contain_file("/opt/logstashforwarder/swdl/package.#{pkg_ext}").with(:source => "/path/to/package.#{pkg_ext}", :backup => false) }
+            it { should contain_package('logstash-forwarder').with(:ensure => 'present', :source => "/opt/logstashforwarder/swdl/package.#{pkg_ext}", :provider => pkg_prov) }
           end
 
         end
@@ -174,33 +188,33 @@ describe 'logstashforwarder', :type => 'class' do
 
           context 'and set defaults via hash param' do
 
-            let (:params) { 
+            let (:params) {
               default_params.merge({
-              :init_defaults => { 'SERVICE_USER' => 'root', 'SERVICE_GROUP' => 'root' }
+                :init_defaults => { 'SERVICE_USER' => 'root', 'SERVICE_GROUP' => 'root' }
               })
             }
 
-            it { should contain_file('/etc/sysconfig/logstash-forwarder').with(:content => "### MANAGED BY PUPPET ###\n\nSERVICE_GROUP=root\nSERVICE_USER=root\n") }
+            it { should contain_file("#{defaults_path}/logstash-forwarder").with(:content => "### MANAGED BY PUPPET ###\n\nSERVICE_GROUP=root\nSERVICE_USER=root\n") }
 
           end
 
           context 'and set defaults via file param' do
 
-            let (:params) { 
+            let (:params) {
               default_params.merge({
-              :init_defaults_file => 'puppet:///path/to/logstashforwarder.defaults'
+                :init_defaults_file => 'puppet:///path/to/logstashforwarder.defaults'
               })
             }
 
-            it { should contain_file('/etc/sysconfig/logstash-forwarder').with(:source => 'puppet:///path/to/logstashforwarder.defaults') }
+            it { should contain_file("#{defaults_path}/logstash-forwarder").with(:source => 'puppet:///path/to/logstashforwarder.defaults') }
 
           end
 
           context 'and set init file via template' do
 
-            let (:params) { 
+            let (:params) {
               default_params.merge({
-              :init_template => 'logstashforwarder/etc/init.d/logstashforwarder.RedHat.erb'
+                :init_template => "logstashforwarder/etc/init.d/logstashforwarder.Debian.erb"
               })
             }
 
@@ -210,35 +224,10 @@ describe 'logstashforwarder', :type => 'class' do
 
           context 'No service restart when restart_on_change is false' do
 
-            let (:params) { 
+            let (:params) {
               default_params.merge({
-              :init_template     => "logstashforwarder/etc/init.d/logstashforwarder.RedHat.erb",
-              :restart_on_change => false
-              })
-            }
-
-            it { should contain_file('/etc/init.d/logstash-forwarder').without_notify }
-
-          end
-
-          context 'when its unmanaged do nothing with it' do
-
-            let (:params) { 
-              default_params.merge({
-              :status => 'unmanaged'
-              })
-            }
-
-            it { should contain_service('logstash-forwarder').with(:ensure => nil, :enable => false) }
-
-          end
-
-          context 'No service restart when restart_on_change is false' do
-
-            let (:params) { 
-              default_params.merge({
-              :init_template     => "logstashforwarder/etc/init.d/logstashforwarder.RedHat.erb",
-              :restart_on_change => false
+                :init_template     => "logstashforwarder/etc/init.d/logstashforwarder.Debian.erb",
+                :restart_on_change => false
               })
             }
 
@@ -250,7 +239,7 @@ describe 'logstashforwarder', :type => 'class' do
 
             let (:params) {
               default_params.merge({
-              :status => 'unmanaged'
+                :status => 'unmanaged'
               })
             }
 
@@ -264,7 +253,7 @@ describe 'logstashforwarder', :type => 'class' do
 
       context 'when setting the module to absent' do
 
-        let (:params) { 
+        let (:params) {
           default_params.merge({
             :ensure => 'absent'
           })
